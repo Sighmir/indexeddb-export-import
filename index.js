6b0ecc0087a727fd6a87bee89a43d2e6b51178e9
Sighmir <guistoppa1995@gmail.com>
2019-12-30 16:09:02 -0300

    Move functions inside const for better browser support

--- index.js
+++ index.js
@@ -2,106 +2,109 @@ isNode = () => {
   return typeof module !== "undefined" && module.exports;
 };
 
-/**
- * Export all data from an IndexedDB database
- * @param {IDBDatabase} idbDatabase - to export from
- */
-function exportToJsonString(idbDatabase) {
-  return new Promise((resolve, reject) => {
-    const exportObject = {};
-    if (idbDatabase.objectStoreNames.length === 0)
-      resolve(JSON.stringify(exportObject));
-    else {
+const IDBExportImport = {
+  /**
+   * Clears a database of all data
+   *
+   * @param {IDBDatabase} idbDatabase - to delete all data from
+   */
+  clearDatabase: idbDatabase => {
+    return new Promise((resolve, reject) => {
       const transaction = idbDatabase.transaction(
         idbDatabase.objectStoreNames,
-        "readonly"
+        "readwrite"
       );
       transaction.onerror = event => {
         reject(event);
       };
+      let count = 0;
       _.forEach(idbDatabase.objectStoreNames, storeName => {
-        const allObjects = [];
-        transaction.objectStore(storeName).openCursor().onsuccess = event => {
-          const cursor = event.target.result;
-          if (cursor) {
-            allObjects.push(cursor.value);
-            cursor.continue();
-          } else {
-            exportObject[storeName] = allObjects;
-            if (
-              idbDatabase.objectStoreNames.length ===
-              _.keys(exportObject).length
-            ) {
-              resolve(JSON.stringify(exportObject));
-            }
-          }
-        };
-      });
-    }
-  });
-}
-
-/**
- * Import data from JSON into an IndexedDB database. This does not delete any existing data
- *  from the database, so keys could clash
- *
- * @param {IDBDatabase} idbDatabase - to import into
- * @param {string} jsonString - data to import, one key per object store
- */
-function importFromJsonString(idbDatabase, jsonString) {
-  return new Promise((resolve, reject) => {
-    const transaction = idbDatabase.transaction(
-      idbDatabase.objectStoreNames,
-      "readwrite"
-    );
-    transaction.onerror = event => {
-      reject(event);
-    };
-    const importObject = JSON.parse(jsonString);
-    _.forEach(idbDatabase.objectStoreNames, storeName => {
-      const count = 0;
-      _.forEach(importObject[storeName], toAdd => {
-        const request = transaction.objectStore(storeName).add(toAdd);
-        request.onsuccess = event => {
+        transaction.objectStore(storeName).clear().onsuccess = () => {
           count++;
-          if (count === importObject[storeName].length) {
-            // added all objects for this store
-            delete importObject[storeName];
-            if (_.keys(importObject).length === 0)
-              // added all object stores
-              resolve(null);
-          }
+          if (count === idbDatabase.objectStoreNames.length)
+            // cleared all object stores
+            resolve(null);
         };
       });
     });
-  });
-}
+  },
 
-/**
- * Clears a database of all data
- *
- * @param {IDBDatabase} idbDatabase - to delete all data from
- */
-function clearDatabase(idbDatabase) {
-  return new Promise((resolve, reject) => {
-    const transaction = idbDatabase.transaction(
-      idbDatabase.objectStoreNames,
-      "readwrite"
-    );
-    transaction.onerror = function(event) {
-      reject(event);
-    };
-    const count = 0;
-    _.forEach(idbDatabase.objectStoreNames, function(storeName) {
-      transaction.objectStore(storeName).clear().onsuccess = function() {
-        count++;
-        if (count === idbDatabase.objectStoreNames.length)
-          // cleared all object stores
-          resolve(null);
-      };
+  /**
+   * Export all data from an IndexedDB database
+   *
+   * @param {IDBDatabase} idbDatabase - to export from
+   */
+  exportToJsonString: idbDatabase => {
+    return new Promise((resolve, reject) => {
+      const exportObject = {};
+      if (idbDatabase.objectStoreNames.length === 0)
+        resolve(JSON.stringify(exportObject));
+      else {
+        const transaction = idbDatabase.transaction(
+          idbDatabase.objectStoreNames,
+          "readonly"
+        );
+        transaction.onerror = event => {
+          reject(event);
+        };
+        _.forEach(idbDatabase.objectStoreNames, storeName => {
+          const allObjects = [];
+          transaction.objectStore(storeName).openCursor().onsuccess = event => {
+            const cursor = event.target.result;
+            if (cursor) {
+              allObjects.push(cursor.value);
+              cursor.continue();
+            } else {
+              exportObject[storeName] = allObjects;
+              if (
+                idbDatabase.objectStoreNames.length ===
+                _.keys(exportObject).length
+              ) {
+                resolve(JSON.stringify(exportObject));
+              }
+            }
+          };
+        });
+      }
     });
-  });
-}
+  },
+
+  /**
+   * Import data from JSON into an IndexedDB database.
+   * This does not delete any existing data from the database, so keys could clash
+   *
+   * @param {IDBDatabase} idbDatabase - to import into
+   * @param {string} jsonString - data to import, one key per object store
+   */
+  importFromJsonString: (idbDatabase, jsonString) => {
+    return new Promise((resolve, reject) => {
+      const transaction = idbDatabase.transaction(
+        idbDatabase.objectStoreNames,
+        "readwrite"
+      );
+      transaction.onerror = event => {
+        reject(event);
+      };
+      const importObject = JSON.parse(jsonString);
+      _.forEach(idbDatabase.objectStoreNames, storeName => {
+        let count = 0;
+        _.forEach(importObject[storeName], toAdd => {
+          const request = transaction.objectStore(storeName).add(toAdd);
+          request.onsuccess = event => {
+            count++;
+            if (count === importObject[storeName].length) {
+              // added all objects for this store
+              delete importObject[storeName];
+              if (_.keys(importObject).length === 0)
+                // added all object stores
+                resolve(null);
+            }
+          };
+        });
+      });
+    });
+  }
+};
 
 if (isNode()) {
   _ = {
@@ -109,9 +112,5 @@ if (isNode()) {
     keys: require("lodash.keys")
   };
 
-  module.exports = {
-    clearDatabase: clearDatabase,
-    exportToJsonString: exportToJsonString,
-    importFromJsonString: importFromJsonString
-  };
+  module.exports = IDBExportImport;
 }
